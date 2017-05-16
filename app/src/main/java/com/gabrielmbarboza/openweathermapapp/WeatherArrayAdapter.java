950fb23749e9c59695bb6f1069f6dc7ecdc81e83
Gabriel Barboza <gabrielmbarboza@gmail.com>
2017-05-15 23:36:29 -0300

    Add list item custom layout

--- app/src/main/java/com/gabrielmbarboza/openweathermapapp/WeatherArrayAdapter.java
+++ app/src/main/java/com/gabrielmbarboza/openweathermapapp/WeatherArrayAdapter.java
@@ -1,19 +1,44 @@
 package com.gabrielmbarboza.openweathermapapp;
 
 import android.content.Context;
-import android.support.annotation.LayoutRes;
+import android.graphics.Bitmap;
 import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.ImageView;
 import android.widget.TextView;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 /**
  * Created by gabriel on 14/05/17.
  */
 
 public class WeatherArrayAdapter extends ArrayAdapter<Weather> {
-    public WeatherArrayAdapter(@NonNull Context context, @LayoutRes int resource) {
-        super(context, resource);
+    private Map<String, Bitmap> bitmaps = new HashMap<>();
+
+    public WeatherArrayAdapter(Context context, List<Weather> weatherForecast) {
+        super(context, -1, weatherForecast);
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        ViewHolder viewHolder;
+        Weather day = getItem(position);
+
+        if(convertView == null) {
+            viewHolder = new ViewHolder();
+            LayoutInflater inflater = LayoutInflater.from(getContext());
+            convertView = inflater.inflate(R.layout.list_item, parent, false);
+        } else {
+            viewHolder = (ViewHolder) convertView.getTag();
+        }
+
     }
 
     private static class ViewHolder {
