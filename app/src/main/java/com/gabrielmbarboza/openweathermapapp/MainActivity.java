48aa53a3325d40d48bee8566338eb812433f77ed
Gabriel Moraes <gmoraes@ie.com.br>
2017-05-18 15:17:24 -0300

    Get Weather in json format is done

--- app/src/main/java/com/gabrielmbarboza/openweathermapapp/MainActivity.java
+++ app/src/main/java/com/gabrielmbarboza/openweathermapapp/MainActivity.java
@@ -1,5 +1,7 @@
 package com.gabrielmbarboza.openweathermapapp;
 
+import android.content.Context;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -9,8 +11,13 @@ import android.util.Log;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.EditText;
 import android.widget.ListView;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 import java.net.URL;
 import java.net.URLEncoder;
@@ -39,14 +46,28 @@ public class MainActivity extends AppCompatActivity {
                 EditText location = (EditText) findViewById(R.id.location_et);
                 String appID = getString(R.string.app_id);
                 String wsUrl = getString(R.string.ws_url);
+                String owmApiUrl = null;
 
                 try {
-                    String owmApiUrl = wsUrl + URLEncoder.encode(location.getText().toString(), "UTF-8")
+                    owmApiUrl = wsUrl + URLEncoder.encode(location.getText().toString(), "UTF-8")
                             + "&units=metric&APPID=" + appID;
+
+                    URL url = new URL(owmApiUrl);
+
+                    if(url != null) {
+                        InputMethodManager inputMethodManager = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
+                        inputMethodManager.hideSoftInputFromWindow(view.getWindowToken(), 0);
+
+                        WeatherTask weatherTask = new WeatherTask();
+                        weatherTask.execute(url);
+                    } else {
+                        Snackbar.make(findViewById(R.id.coordinatorLayout), R.string.invalid_url, Snackbar.LENGTH_LONG).show();
+                    }
                 } catch (Exception e) {
                     Log.e("MainActivity", e.getMessage(), e);
                 }
-                //Url url =
+
+
             }
         });
     }
@@ -71,5 +92,43 @@ public class MainActivity extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    private class WeatherTask extends AsyncTask<URL, Void, JSONObject> {
+        @Override
+        protected JSONObject doInBackground(URL... params) {
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(JSONObject weather) {
+           getJsonToArrayList(weather);
+            weatherArrayAdapter.notifyDataSetChanged();
+            weatherLV.smoothScrollToPosition(0);
+        }
+
+        private void getJsonToArrayList(JSONObject weatherForecast) {
+            weatherList.clear();
+
+            try {
+                JSONArray list = weatherForecast.getJSONArray("list");
+
+                for (int i = 0; i < databaseList().length; i++) {
+                    JSONObject day = list.getJSONObject(i);
+                    JSONObject temperatures = day.getJSONObject("temp");
+                    JSONObject weather = day.getJSONArray("weather").getJSONObject(0);
+
+                    weatherList.add(new Weather(
+                            day.getLong("dt"),
+                            temperatures.getDouble("max"),
+                            temperatures.getDouble("min"),
+                            weather.getString("description"),
+                            weather.getString("icon")
+                    ));
+                }
+            } catch (JSONException e) {
+                Log.e("MainActivity", e.getMessage(), e);
+            }
+        }
     }
 }
